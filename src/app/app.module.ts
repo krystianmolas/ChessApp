506808c48a17c51fc0667c0a76e4ec2517ef0e84
Krystian Mołas <krystian.molas1@gmail.com>
2023-08-10 23:51:28 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PieceListComponent } from './components/piece-list/piece-list.component';
+import { ChessboardComponent } from './components/chessboard/chessboard.component';
+import { MovesListComponent } from './components/moves-list/moves-list.component';
+import { AlertComponent } from './components/alert/alert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieceListComponent,
+    ChessboardComponent,
+    MovesListComponent,
+    AlertComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
